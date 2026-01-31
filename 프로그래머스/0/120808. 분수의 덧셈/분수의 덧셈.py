cdb5e4520e81882446701c872f2f00cc38f4f545
MunSu2001 <anstn3375@naver.com>
2026-01-31 21:23:30 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,9 @@
+from math import gcd
+def solution(numer1, denom1, numer2, denom2):
+    answer = []
+    a = numer1 * denom2 + numer2 * denom1
+    b = denom1 * denom2
+    g = gcd(a, b)
+    answer.append(a//g)
+    answer.append(b//g)
+    return answer
